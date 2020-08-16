de924760d3841e4dcd79eaaaa95134b9178be0e5
Morihide Saegusa <morihide.saegusa@gmail.com>
2020-08-17 00:25:14 +0900

    add file.

--- uno/src/test/java/uno/MainTest.java
+++ uno/src/test/java/uno/MainTest.java
@@ -8,5 +8,21 @@ class MainTest {
 	void testMain() {
 		Main.main(null);
 	}
+	
+	
+	/**
+	 * Java 14 spec test.
+	 * @see https://qiita.com/taumax/items/4208d5cdbb786b76a5ed
+	 */
+	@Test
+	public void JEP35() {
+		var a = """
+				abc
+				test
+				アイウエオ
+				かきくけこ
+				""";
+		System.out.println(a);
+	}
 
 }
