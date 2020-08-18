72fecec0981df089ed9610026e7f129f741eba88
Morihide Saegusa <morihide.saegusa@gmail.com>
2020-08-19 01:30:13 +0900

    1: Task List は便利？
    Task-Url: https://github.com/saegusa/troublesome/issues/1
    
    これはお試し、MylynのGithub　Connectorを利用すると
    行っているタスクに合わせた作業セットを指定できるようだ。

--- uno/src/test/java/uno/MainTest.java
+++ uno/src/test/java/uno/MainTest.java
@@ -1,28 +1,58 @@
 package uno;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * Java 14 spec test.
+ * 
+ * @see https://qiita.com/taumax/items/4208d5cdbb786b76a5ed
+ */
+@SuppressWarnings("preview")
 class MainTest {
 
 	@Test
-	void testMain() {
+	void testMain () {
 		Main.main(null);
-	}
-	
-	
-	/**
-	 * Java 14 spec test.
-	 * @see https://qiita.com/taumax/items/4208d5cdbb786b76a5ed
-	 */
-	@Test
-	public void JEP35() {
-		var a = """
+		System.out.println("""
 				abc
 				test
 				アイウエオ
 				かきくけこ
-				""";
-		System.out.println(a);
+				""");
+	}
+
+	public static Stream< Object > provideData () {
+		return Stream.of("Test", Integer.valueOf(200), Long.valueOf(300));
+	}
+
+	/**
+	 * JEP 305：Pattern Matching for instanceof (Preview)
+	 */
+	@ParameterizedTest
+	@MethodSource("provideData")
+	public void JEP305 ( Object obj ) {
+
+		// これまでの書き方
+		if (obj instanceof String) {
+			String str = (String) obj;
+			System.out.println("No Pattern Matching:" + str);
+		}
+		else if (obj instanceof Integer) {
+			Integer inte = (Integer) obj;
+			System.out.println("No Pattern Matching:" + inte);
+		}
+
+		// Pattern Matching for instanceof
+		if (obj instanceof String str) {
+			System.out.println("Pattern Matching:" + str);
+		}
+		else if (obj instanceof Integer inte) {
+			System.out.println("Pattern Matching:" + inte);
+		}
 	}
 
 }
